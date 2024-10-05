1c7cf97ce75068b963500d31f9d12b02f054b376
Luiz Felipe Fonseca Rosa <luizfelipefonsecarosa@gmail.com>
2024-10-05 16:54:34 -0300

    boilerplate 1

--- main.py
+++ main.py
@@ -3,3 +3,43 @@ import pandas as pd
 data = pd.read_csv('./data/qsar_fish_toxicity.csv')
 
 print(data.head)
+
+# To each one, run 20 times and get:
+    # - Mean Average Error
+    # - Mean Squared Error
+    # - Root Mean Squared Error
+
+## KNR
+    # - K
+    # - Distance
+
+
+## SVR
+    # - Kernel
+    # - C
+
+
+## MLP
+    # - Hidden Layer
+    # - Activation Function
+    # - Max Iterations
+    # - Learning Rate
+
+
+## RF
+    # - N Estimators
+    # - Max Depth
+    # - Min Samples Split
+    # - Min Samples Leaf
+    # - Criterion
+
+## GB
+    # - N Estimators
+    # - Learning Rate
+    # - Loss
+    # - Max Depth
+    # - Min Samples Split
+    # - Min Samples Leaf
+
+## RLM
+    # - None
